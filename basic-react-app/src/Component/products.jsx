9e965abf7a42581fddd87361ba5705df6cdf4a22
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-06-05 16:07:04 +0530

    add_like_button

--- basic-react-app/src/Component/products.jsx
+++ basic-react-app/src/Component/products.jsx
@@ -2,6 +2,10 @@ import React,{useState} from 'react'
 import ProCounter from './ProCounter';
 const Products = ({title,price}) => {
     const [count, setCount] = useState(0);
+    const [isLike , setIsLike] = useState(false)
+    function toggle(){
+         setIsLike(!isLike)
+    }
 
     const handleIncrement = () => {
         setCount(count + 1);
@@ -19,12 +23,22 @@ const Products = ({title,price}) => {
                 <div className="card-body">
                     <h4 className="card-title"> {title}</h4>
                     <p className="card-text">Price : {price}</p>
+                    <p onClick = {toggle}>
+                        {
+                            isLike ? (
+                               <i className="fa-solid fa-heart"></i>
+                            ) : (
+                            <i className="fa-regular fa-heart"></i>
+                        ) 
+                        }
+                    </p>
                     <h6>{count}</h6>
                 <button onClick={handleIncrement}>plus</button>
                 <button onClick={handleDecrement}>minus</button>
                 </div>
             </div>
             <ProCounter count={count}/>
+            <ProCounter count={isLike}/>
         </>
     )
 }

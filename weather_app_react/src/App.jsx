7dd2640f1f59daf3c80ec16aaa7fe842f8493425
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-06-17 11:23:41 +0530

    build_weather_func_

---
+++ weather_app_react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchBox from './Component/SearchBox'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <SearchBox/>
+    </>
+  )
+}
+
+export default App

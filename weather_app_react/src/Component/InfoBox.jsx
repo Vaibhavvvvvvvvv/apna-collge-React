8884334d84e85ff314358ec494956bd724c5e9a3
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-06-21 19:35:23 +0530

    add_default_weather_functinality

--- weather_app_react/src/Component/InfoBox.jsx
+++ weather_app_react/src/Component/InfoBox.jsx
@@ -1,49 +1,40 @@
-import React from 'react'
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
-const InfoBox = ({info}) => {
-   const int_url = "https://i.gifer.com/68J.gif"
-    
-    let hot_url = "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0"
-    let cold_url ="https://images.unsplash.com/photo-1541781774459-3f93c625d05c"
-    let rain_url = "https://images.unsplash.com/photo-1445810694374-00c7b6374a6b" 
-    return (
-        <Card sx={{ maxWidth: 300}}>
+const InfoBox = ({ info }) => {
+  
+  // Provide default values to avoid undefined errors
+  const { city = 'Unknown', feelsLike = 0, humidity = 0, temp = 0, tempMax = 0, tempMin = 0, weather = 'Unknown' } = info || {};
+
+  return (
+    <Card sx={{ maxWidth: 300 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="100"
-          image = {
-            info.humidity > 80 
-            ? rain_url
-            : info.temp > 15 
-            ? hot_url
-            : cold_url
-          }
+          image="https://store-images.s-microsoft.com/image/apps.16894.c02476d2-2378-4f60-8290-b6d4b3842643.79a2ef6a-4775-4c79-8d93-9caf077660eb.1bbd88a4-0a17-4750-91a0-cd7d98f58e9d"
           alt="Weather"
         />
         <CardContent>
           <Typography gutterBottom variant="h4" component={"span"}>
-          <h6>city: {info.city}</h6>
-           </Typography>
-           <Typography variant="body2" color="text.secondary">
-           <p>feelsLike: {info.feelsLike}</p>
-           <p>humidity: {info.humidity}</p>
-           <p>temp: {info.temp}</p>
-           <p>tempmax: {info.tempMax}</p>
-           <p>tempmin: {info.tempMin}</p>
-           <p>weather:{info.weather}</p>
-           </Typography>
-           </CardContent>
-           </CardActionArea>
-           </Card>
-       
-    )
+            <h6>city: {city}</h6>
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            <p>feelsLike: {feelsLike}</p>
+            <p>humidity: {humidity}</p>
+            <p><b>temp: {temp}</b></p>
+            <p>tempmax: {tempMax}</p>
+            <p>tempmin: {tempMin}</p>
+            <p>weather: {weather}</p>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
-export default InfoBox
 
-
+export default InfoBox;

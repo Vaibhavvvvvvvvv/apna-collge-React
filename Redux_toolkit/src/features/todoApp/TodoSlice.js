7b88d120f7032b4fb98af911b9c145b56880dfb3
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-06-22 13:51:07 +0530

    redux_setup

---
+++ Redux_toolkit/src/features/todoApp/TodoSlice.js
@@ -0,0 +1,37 @@
+//reducers
+import { createSlice , nanoid } from '@reduxjs/toolkit'
+
+const intialState = {
+    todos:[{
+        id:"abc",
+        task: "demo-task",
+        isDone:false
+    }],
+}
+
+export const todoSlice = createSlice({
+    name:"todo",
+    intialState,
+    reducers:{
+        addTodo:(state , action)=>{
+            const newTodo={
+                id:nanoid(),
+                task:action.payload,
+                isDone:false
+            }
+            state.todos.push(newTodo)
+        },
+        deleteTodo:(state, action)=>{
+          state.todos = state.todos.filter((todo)=>todo.id != action.payload);
+        },
+        markAsDone:(state , action)=>{
+            state.todos = state.todos.map((todo)=>{
+                if(todo.id === action.payload){
+                    todo.isDone = true
+                }
+            })          
+        }
+    },
+})
+export const {addTodo,deleteTodo,markAsDone} = todoSlice.actions;
+export default todoSlice.reducer;
---
+++ Redux_toolkit/src/reduxSetup/Store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit'
+import todoReducer from "../features/todoApp/TodoSlice"
+export  const store = configureStore({
+    reducer:todoReducer,
+})
+    
